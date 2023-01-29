0a28a2af826326b236620436beeb54e6991c1fae
Thomas Kaar <thomas.kaar@verizonmedia.com>
2023-01-28 23:05:05 -0800

    add bubble style

--- master.js
+++ master.js
@@ -79,3 +79,22 @@ document.getElementById('coder-name').onmouseover = event => {
         iteration += 1 / 3;
     }, 30);
 };
+
+/** Begin bubble animation js */
+const wrapper = document.getElementById("bubble-wrapper");
+
+const animateBubble = x => {
+    const bubble = document.createElement('div');
+
+    bubble.className = "bubble";
+
+    bubble.style.left = `${x}px`;
+
+    wrapper.appendChild(bubble);
+
+    bubble.style.background = 'linear-gradient(90deg, #b14dc3 0%, #f30658 76%)'
+
+    setTimeout(() => wrapper.removeChild(bubble), 2000);
+};
+
+window.onmousemove = e => animateBubble(e.clientX);
